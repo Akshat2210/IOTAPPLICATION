b31924653ad2be9dfffb18659ab40fb6f7d05a48
Akshat2210 <82823468+Akshat2210@users.noreply.github.com>
2023-10-25 00:52:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; // You can change the port number if needed
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hi Akshat');
+app.use(bodyParser.json());
+
+app.post('/control', (req, res) => {
+  // Handle control signals sent from the Android app
+  const controlData = req.body; // Assuming the app sends JSON data
+  // Send control signals to the ESP32
+  // You may use a library like 'axios' to send requests to the ESP32
+  // You can also send a response back to the app for confirmation
+
+  res.json({ status: 'Control signals sent to ESP32' });
 });
 
 app.listen(port, () => {
