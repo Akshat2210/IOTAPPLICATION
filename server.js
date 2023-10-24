8c097b9b9578d2dfe6abd2842f95b2351f8afa5b
Akshat2210 <82823468+Akshat2210@users.noreply.github.com>
2023-10-25 00:59:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Hello, this is the root path.');
+});
 
 app.post('/control', (req, res) => {
   // Handle control signals sent from the Android app
