b06e8d64bb77cd37707a9c80fa7db397e4a42bfe
Akshat2210 <82823468+Akshat2210@users.noreply.github.com>
2023-10-24 23:51:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Serve static files (like HTML, CSS, and JavaScript) from the "public" directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
