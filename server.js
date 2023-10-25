153aa360e5c21dc68072991bb8ed6a90d0d5c663
Akshat2210 <82823468+Akshat2210@users.noreply.github.com>
2023-10-25 12:41:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<div id="direction">No direction received</div>');
+});
+
 const wss = new WebSocket.Server({ server });
 
-let currentDirection = '';
-let clients = new Set(); // Store connected clients
+let currentDirection = 'No direction received';
 
 wss.on('connection', (ws) => {
-  // Add the connected client to the set
-  clients.add(ws);
+  ws.send(currentDirection);
 
-  // Send the current direction to the connected client
-  ws.send(JSON.stringify({ direction: currentDirection }));
-
-  // Handle messages from the client (Java application)
   ws.on('message', (message) => {
-    // Update the current direction when a message is received
-    const controlData = JSON.parse(message);
-    currentDirection = controlData.direction;
-
-    // Broadcast the updated direction to all connected clients
-    clients.forEach((client) => {
+    currentDirection = message;
+    wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ direction: currentDirection }));
+        client.send(currentDirection);
       }
     });
   });
-
-  // Handle client disconnection
-  ws.on('close', () => {
-    clients.delete(ws);
-  });
 });
-
-// Serve an HTML page with the current direction
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.use(express.static('public'));
 
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
